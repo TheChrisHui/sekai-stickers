7b45847fd702c770f816b1468b41b14b02f918f9
thechrishui <huipokshingchris@hotmail.com>
2025-07-25 18:16:07 +0800

    Fixed Database Configuration

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
-const {Pool} = require("pg")
+const {Pool} = require("pg");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const pool = new Pool({
-    user: 'postgres',
-    database: "stickers_db",
-    password: "",
-    port: "5432",
-    host: "localhost"
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST
 })
 
 module.exports = pool
